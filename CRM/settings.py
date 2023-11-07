f1f0d714c67b21d842fba5c07e8f52920b6e896b
hamdan965 <83244096+hamdan965@users.noreply.github.com>
2023-11-07 12:18:30 +0530

    server issues

--- CRM/settings.py
+++ CRM/settings.py
@@ -33,7 +33,8 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['crm-production-41dc.up.railway.app']
+ALLOWED_HOSTS = ['crm-production-41dc.up.railway.app', '*']
+
 
 
 # Application definition
